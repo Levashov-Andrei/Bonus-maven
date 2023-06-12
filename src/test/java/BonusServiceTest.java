d4f8cdb61f23fb3ed1c1bff6dcc2c99af5ec9da5
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-06-12 21:39:06 +0300

    Fix #1

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -59,7 +59,7 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = false;
-        long expected = 450;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
